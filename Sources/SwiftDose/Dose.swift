0769b5223a7da3171f332c962efe9fb439881a1e
Artur Hellmann <a.hellmann@Asset-10282.fritz.box>
2023-02-09 13:29:47 +0100

    fixed private and public issues

--- Sources/SwiftDose/Dose.swift
+++ Sources/SwiftDose/Dose.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 @propertyWrapper
-struct Dose<T> {
+public struct Dose<T> {
     private let keyPath: WritableKeyPath<DoseValues, T>
-    var wrappedValue: T {
+    public var wrappedValue: T {
         get { DoseValues[keyPath] }
         set { DoseValues[keyPath] = newValue }
     }
     
-    init(_ keyPath: WritableKeyPath<DoseValues, T>) {
+    public init(_ keyPath: WritableKeyPath<DoseValues, T>) {
         self.keyPath = keyPath
     }
 }
--- Sources/SwiftDose/DoseValues.swift
+++ Sources/SwiftDose/DoseValues.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 /// Provides access to injected dependencies.
-struct DoseValues {
+public struct DoseValues {
     
     /// This is only used as an accessor to the computed properties within extensions of `DoseValues`.
     private static var current = DoseValues()
     
     /// A static subscript for updating the `currentValue` of `DoseKey` instances.
-    static subscript<K>(key: K.Type) -> K.Value where K : DoseKey {
+    public static subscript<K>(key: K.Type) -> K.Value where K : DoseKey {
         get { key.currentValue }
         set { key.currentValue = newValue }
     }
     
     /// A static subscript accessor for updating and references dependencies directly.
-    static subscript<T>(_ keyPath: WritableKeyPath<DoseValues, T>) -> T {
+    public static subscript<T>(_ keyPath: WritableKeyPath<DoseValues, T>) -> T {
         get { current[keyPath: keyPath] }
         set { current[keyPath: keyPath] = newValue }
     }
